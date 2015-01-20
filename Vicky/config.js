885e48b989b401a855782a7447f6963cea77126a
Ivan Bacher <ivan.bacher@gmail.com>
2015-01-20 14:49:15 +0000

    save

--- Vicky/config.js
+++ Vicky/config.js
@@ -9,6 +9,8 @@ System.config({
 System.config({
   "map": {
     "stats.js": "github:mrdoob/stats.js@master",
-    "three.js": "github:mrdoob/three.js@master"
+    "three.js": "github:mrdoob/three.js@master",
+    "threex.text": "github:jeromeetienne/threex.text@master"
   }
 });
+
